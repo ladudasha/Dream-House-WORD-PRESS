9e52a01a5a00a97fa9f4e3d096de0744afbf5ef1
ladudasha <darya.gameza@gmail.com>
2022-02-12 15:42:17 +0300

    scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,21 @@ burgerMenu.classList.toggle("burger__menu-active")
 }
 
 toggleBurgerMenu()
+
+
+
+
+// плавный скролл
+let anchors = document.querySelectorAll("a[href*='#']")
+console.log(anchors);
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(event) {
+    event.preventDefault()
+    
+    let blockID  = anchor.getAttribute('href');
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
